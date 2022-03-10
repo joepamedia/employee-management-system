dd70ddf4ab8375e1c4940b7848a81af0669f9009
joepamedia <josephpalmisano18@gmail.com>
2022-03-10 14:10:17 -0600

    seeds

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (name)
+VALUES ("Production"),
+        ("Marketing"),
+        ("Sales");
+
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Writer", 100000, 1),
+        ("Graphic Designer", 100000, 2),
+        ("Accountant", 100000, 3),
+        ("Production Manager", 150000, 1),
+        ("Marketing Manager", 150000, 2),
+        ("Sales Manager", 150000, 3);
+    
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Joe", "Pa", 4, NULL),
+        ("Sam", "Adams", 1, 1),
+        ("Hayley", "Smith", 6, NULL ),
+        ("Quentin", "Tarantino", 3, 3);
+
